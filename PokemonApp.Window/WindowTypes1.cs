f38d110ea3d0b92ab64c5bdc7e142429aa886a1c
denpadokei <jsoldenpa@yahoo.co.jp>
2019-12-07 18:22:55 +0900

    テキストテンプレート追加（ただし絶対パス）

---
+++ PokemonApp.Window/WindowTypes1.cs
@@ -0,0 +1,23 @@
+﻿using PokemonApp.AbilityValueConverter.Views;
+using PokemonApp.Damage.Views;
+using PokemonApp.Json.Views;
+using PokemonApp.PictureBook.Views;
+using PokemonApp.WildArea.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PokemonApp.WindowManage
+{
+    public partial class WindowType
+    {
+		public static WindowType PictuerBook => new WindowType("ポケモンのデータあれこれ", nameof(PokemonDataView));
+		public static WindowType DamageSim => new WindowType("ダメージ計算", nameof(DamageSimView));
+		public static WindowType WildArea => new WindowType("ワイルドエリア一覧", nameof(WildAreaView));
+		public static WindowType AbilityValueConverter => new WindowType("能力値計算機", nameof(AbilityValueConverterView));
+		public static WindowType JsonSerch => new WindowType("JSONデータ探索", nameof(JsonSerchView));
+	}
+}
